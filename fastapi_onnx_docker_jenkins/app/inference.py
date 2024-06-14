fe2ab2ec2df1054a484b4a1f76ea339a41adfefd
byoungjuchae <wpdhkd1642@gmail.com>
2024-06-14 13:40:52 +0900

    first commit

--- fastapi_onnx_docker_jenkins/app/inference.py
+++ fastapi_onnx_docker_jenkins/app/inference.py
@@ -2,11 +2,15 @@ import onnxruntime as ort
 from fastapi import FastAPI, File
 from PIL import Image
 import io
+from model import CNN
+import torchvision.transforms as T
 
-
-
+providers= ["CUDAExecutionProvider"]
+input_transform = T.Compose([T.Resize(224),
+                             T.ToTensor(),
+                             T.Normalize(mean=[0.485,0.456,0.406],std=[0.229,0.224,0.225])])
 app = FastAPI()
-
+model = CNN().cuda()
 
 @app.post('/predict/')
 async def predicton(file : bytes = File(...)) :
